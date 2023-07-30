37fe5a1966839e8ac2a8ce61753e7804021d26fa
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-07-30 07:41:10 +0300

    updateStatusContact err added

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -49,9 +49,15 @@ const updateContactById = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
     const { contactId } = req.params;
+    const { favorite } = req.body;
+
+    if (!favorite) {
+        return res.status(400).json({ message: "missing field favorite" });
+    }
+
     const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true })
     if (!result) {
-    throw HttpError(404, "Missing field favorite" )
+    throw HttpError(404, "Not found" )
     }
     res.json(result)
 }
